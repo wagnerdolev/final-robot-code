08252c302f525944d86c3b8f3f7b51420cd97fd7
DolevWagner <wagnerdolev@gmail.com>
2023-10-19 13:56:07 +0300

    Started Arm subsystem

---
+++ robotc code/src/main/java/frc/robot/Subsystems/Arm.java
@@ -0,0 +1,58 @@
+package frc.robot.Subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.Utils.Consts;
+public class Arm {
+    private CANSparkMax m_firstArmMotor, m_secondArmMotor;
+    private static Arm m_Arm;
+
+    public Arm(){
+        m_firstArmMotor = new CANSparkMax(Consts.ArmConsts.FIRST_ARM_MOTOR_ID, MotorType.kBrushless);
+        m_secondArmMotor = new CANSparkMax(Consts.ArmConsts.SECOND_ARM_MOTOR_ID, MotorType.kBrushless);
+
+        m_firstArmMotor.restoreFactoryDefaults();
+        m_secondArmMotor.restoreFactoryDefaults();
+
+        // config pid values of motor controllers
+        m_firstArmMotor.getPIDController().setP(Consts.ArmConsts.FIRST_ARM_KP);
+        m_firstArmMotor.getPIDController().setI(Consts.ArmConsts.FIRST_ARM_KI);
+        m_firstArmMotor.getPIDController().setD(Consts.ArmConsts.FIRST_ARM_KD);
+        m_firstArmMotor.getPIDController().setFF(Consts.ArmConsts.FIRST_ARM_KF);
+
+        m_secondArmMotor.getPIDController().setP(Consts.ArmConsts.SECOND_ARM_KP);
+        m_secondArmMotor.getPIDController().setI(Consts.ArmConsts.SECOND_ARM_KI);
+        m_secondArmMotor.getPIDController().setD(Consts.ArmConsts.SECOND_ARM_KD);
+        m_secondArmMotor.getPIDController().setFF(Consts.ArmConsts.SECOND_ARM_KF);
+
+        m_firstArmMotor.getEncoder().setPositionConversionFactor(Consts.ArmConsts.FIRST_ARM_GEAR_RATIO * 360);
+        m_secondArmMotor.getEncoder().setPositionConversionFactor(Consts.ArmConsts.SECOND_ARM_GEAR_RATIO * 360);
+
+    }
+
+    public static Arm getInstance(){
+        if(m_Arm == null){
+            m_Arm= new Arm();
+        }
+        return m_Arm;
+    }
+
+    public void turnFirstTo(double angle){
+        m_firstArmMotor.getPIDController().setReference(angle, ControlType.kPosition);
+    }
+
+    public void turnSecondTo(double angle){
+        m_secondArmMotor.getPIDController().setReference(angle, ControlType.kPosition);
+    }
+
+    
+    public double getFirstArmAngle(){
+        return m_firstArmMotor.getEncoder().getPosition();
+    }
+
+    public double getSecondArmAngle(){
+        return m_secondArmMotor.getEncoder().getPosition();
+    }
+}
--- robotc code/src/main/java/frc/robot/Utils/Consts.java
+++ robotc code/src/main/java/frc/robot/Utils/Consts.java
@@ -1,5 +1,23 @@
 package frc.robot.Utils;
 
 public class Consts {
-    
+    public class ArmConsts{
+        public static final int FIRST_ARM_MOTOR_ID = 0;
+        public static final int SECOND_ARM_MOTOR_ID = 0;
+
+        public static final int FIRST_ARM_KP = 0;
+        public static final int FIRST_ARM_KI = 0;
+        public static final int FIRST_ARM_KD = 0;
+        public static final int FIRST_ARM_KF = 0;
+
+        public static final int SECOND_ARM_KP = 0;
+        public static final int SECOND_ARM_KI = 0;
+        public static final int SECOND_ARM_KD = 0;
+        public static final int SECOND_ARM_KF = 0;
+
+        public static final int FIRST_ARM_GEAR_RATIO = 0;
+        public static final int SECOND_ARM_GEAR_RATIO = 0;
+
+
+    }
 }
