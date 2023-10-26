34738454b30ba3f8444badd0cb0ac79564de0bd2
DolevWagner <wagnerdolev@gmail.com>
2023-10-26 20:18:44 +0300

    finished claw commands

--- robotc code/src/main/java/frc/robot/Commands/Claw/closeToCone.java
+++ robotc code/src/main/java/frc/robot/Commands/Claw/closeToCone.java
@@ -3,20 +3,20 @@ package frc.robot.Commands.Claw;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.SubSystems.Claw;
 
-public class openClaw extends CommandBase {
+public class closeToCone extends CommandBase {
 
     public void initialize() {
-        if (!Claw.getInstance().getlimitSwitchOpenBool()) {
-            Claw.getInstance().openClaw();
+        if (Claw.getInstance().getlimitSwitchConeBool()) {
+            Claw.getInstance().closeClaw();
         }
     }
 
     public boolean isFinished() {
-        return Claw.getInstance().getlimitSwitchOpenBool();
-
+        return !Claw.getInstance().getlimitSwitchConeBool();
     }
 
     public void end() {
         Claw.getInstance().stopClaw();
     }
+
 }
